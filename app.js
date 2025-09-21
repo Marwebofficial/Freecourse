3bb62f145cb407c934d8e62c3b7fc912ae0776bb
Marwebofficial <141563399+Marwebofficial@users.noreply.github.com>
2025-09-21 19:13:52 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-let pro = prompt("what is your name");
-alert(pro);
+let user = ["dave", "sam"];
+let request = prompt("Enter your name");
+if (request == user) {
+  alert(`Welcome : ${request}`);
+} else if (request != user) {
+  user.push(request);
+  alert(`you were added : ${request}`);
+} else {
+  alert("you are not allowed");
+}
