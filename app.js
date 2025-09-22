19ea6cf0afef1f808f24c3c7a12efa00a9d343d3
Marwebofficial <141563399+Marwebofficial@users.noreply.github.com>
2025-09-22 09:27:53 +0100

    update app

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-let user = ["james","jane"];
+let user = ["james", "jane"];
 const request = prompt(`Enter your registered name`);
 let create = user.push(request);
 if (user === request) {
   exe();
-} else if (user!= request) {
-  prompt(`you are not registered pls register`);
+} else if (user != request) {
+  let added = prompt(`you are not registered pls register`);
   exe2();
 } else {
   exe3();
@@ -14,7 +14,7 @@ function exe() {
   alert(`Welcome back ${request}`);
 }
 function exe2() {
-  alert(`${request} you were just registered`);
+  alert(`${added} you were just registered`);
 }
 function exe3() {
   alert(`you cant leave input empty`);
